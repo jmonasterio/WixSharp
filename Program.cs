75c748ca931f4e4a8111ad64148181a667f8bb6c
jmfcpa <jmfcpa@logonpro.com>
2015-12-03 13:56:05 -0800

    New prototype

---
+++ Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+// TODO: Emit tags should all be extension methods, so AST separate from emission.
+
+namespace WixSharp
+{
+    public static class Extensions
+    {
+        public static void With<T>(this T input, Action<T> action)
+        {
+            action(input);
+        }
+
+        public static void Set(this object obj, params Func<string, object>[] hash)
+        {
+            foreach (Func<string, object> member in hash)
+            {
+                var propertyName = member.Method.GetParameters()[0].Name;
+                var propertyValue = member(string.Empty);
+                obj.GetType()
+                    .GetProperty(propertyName)
+                    .SetValue(obj, propertyValue, null);
+            }
+            ;
+        }
+    }
+
+    public static class Constants
+    {
+        public static readonly WxGuid ProductGuid = new WxGuid();
+    }
+
+
+    public interface IChildOf<T>
+    {
+    }
+
+    public class WixTag
+    {
+    }
+
+    public class WixLeafTag : WixTag
+    {
+        // Really needed?
+    }
+
+
+    public class WixNodeTag : WixTag
+    {
+        public WixNodeTag() 
+        {
+            Children = new List<WixTag>();
+        }
+
+        public List<WixTag> Children { get; set; }
+
+        public WixRawXml Add(WixRawXml raw)
+        {
+            Children.Add(raw);
+            return raw;
+        }
+    }
+
+
+    public class WixDoc : WixNodeTag
+    {
+        public WixProduct Add(WixProduct product)
+        {
+            Children.Add(product);
+            return product;
+        }
+
+
+    }
+
+    public class WxGuid
+    {
+    }
+
+    public class WixRawXml : WixTag
+    {
+        public string InnerXml { get; set; }
+    }
+
+    public class WixProduct : WixNodeTag, IChildOf<WixDoc>
+    {
+        // TBD: Need reasonable defaults.
+
+        public WxGuid Id { get; set; }
+
+        public string Language { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string Name { get; set; }
+
+        public string UpgradeCode { get; set; }
+
+        public string Version { get; set; }
+
+        public WixPackage Add(WixPackage wixPackage)
+        {
+            Children.Add(wixPackage);
+            return wixPackage;
+        }
+
+
+    }
+
+    public class WixPackage : WixLeafTag, IChildOf<WixProduct>
+    {
+        public string Compressed { get; set; }
+        public string InstallerVersion { get; set; }
+
+    }
+
+
+
+    public static class WixEmitter
+    {
+        public static void EmitFile(this WixDoc doc, FileInfo fileInfo)
+        {
+            using (var stream = fileInfo.CreateText())
+            {
+                var ctx = new EmitContext(doc, stream);
+                Emit(doc, ctx);
+            }
+        }
+
+        public class EmitContext
+        {
+            public WixDoc Root { get; private set; }
+            public WixNodeTag ParentTag { get; set; }
+            public StreamWriter Writer { get; private set; }
+
+            public EmitContext(WixDoc root, StreamWriter sw)
+            {
+                Root = root;
+                ParentTag = null;
+                Writer = sw;
+            }
+        }
+
+        public static void Emit(this WixRawXml rawXml, EmitContext ctx)
+        {
+            ctx.Writer.Write(rawXml.InnerXml);
+        }
+
+        public static void Emit(this WixProduct product, EmitContext ctx)
+        {
+            ctx.Writer.Write("<product >");
+
+            EmitSelfChildren(product, ctx);
+
+            ctx.Writer.Write("</product >");
+        }
+
+        public static void Emit(this WixPackage package, EmitContext ctx)
+        {
+            ctx.Writer.Write("<package >");
+
+            // Can't have children -- typesafe.
+            //EmitSelfChildren( package, ctx);
+
+            ctx.Writer.Write("</package >");
+        }
+
+        public static void Emit(this WixDoc doc, EmitContext ctx)
+        {
+            ctx.Writer.Write("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+            ctx.Writer.Write("<Wix xmlns=\"http://schemas.microsoft.com/wix/2006/wi\">");
+
+             EmitSelfChildren(doc, ctx);
+
+             ctx.Writer.Write("</Wix>");
+        }
+
+
+        public static void EmitSelfChildren(this WixNodeTag node, EmitContext ctx)
+        {
+            var oldParent = ctx.ParentTag;
+            ctx.ParentTag = node;
+
+            if (node.Children != null)
+            {
+                foreach (var child in node.Children)
+                {
+                    Emit(((dynamic)child), ctx); // This can fail if you don't have an override for every time.
+                }
+            }
+
+
+            ctx.ParentTag = oldParent;
+        }
+
+    }
+
+    internal class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            var wix = new WixDoc();
+            var product = wix.Add(new WixProduct() {Id = Constants.ProductGuid, Language = "1033"}); // Yucky constructor.
+            var package = product.Add(new WixPackage() {});
+            product.Add(new WixRawXml() { InnerXml = @"<test />" });
+
+            wix.EmitFile(new FileInfo(@"c:\temp\test.wxs"));
+
+
+        }
+    }
+
+
+}
